9ea2c662707405056c17dcdcf56bbb12f8b402d2
gn0mesort <gnomesort@megate.ch>
2016-11-11 21:25:58 -0800

    Signed-off-by: gn0mesort <gnomesort@megate.ch>

--- js/megatech.js
+++ js/megatech.js
@@ -5,7 +5,20 @@ megatech = function(){
         }
     };
 
+    handleNavBar = function(navbar){
+        var currentLocation = document.location;
+        var navLinks = navbar.getElementsByTagName("a");
+
+        for(var i = 0; i < navLinks.length; ++i){
+            console.log(navLinks[i].href);
+            if(document.location.href === navLinks[i].href){
+                navLink[i].href = "#";
+            }
+        }
+    };
+
     return {
         navigate,
+        handleNavBar
     };
 }();
