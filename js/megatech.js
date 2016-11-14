ddcae99c90b23aeea84bf5e0efccac738f752ac4
gn0mesort <gnomesort@megate.ch>
2016-11-13 21:09:15 -0800

    Signed-off-by: gn0mesort <gnomesort@megate.ch>

--- js/megatech.js
+++ js/megatech.js
@@ -28,3 +28,14 @@ megatech = function(){
         cycleSpinner
     };
 }();
+
+String.prototype.hashCode = function(){
+    var hash = 0;
+    if(this.length == 0){ return hash; }
+    for(var i = 0; i < this.length; ++i){
+        var char = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+    return hash;
+}
