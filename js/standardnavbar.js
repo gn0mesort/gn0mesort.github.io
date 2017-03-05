8106c6fc9bf35cd500c96fd84a259c1bf9d01f78
gn0mesort <gnomesort@megate.ch>
2017-03-05 11:42:14 -0800

    and again
    Signed-off-by: gn0mesort <gnomesort@megate.ch>

--- js/standardnavbar.js
+++ js/standardnavbar.js
@@ -1,18 +1,18 @@
-$.getScript('./js/navbar.js', function () {
-  let navbar = new Navbar('./img/logo.png')
+$.getScript('../js/navbar.js', function () {
+  let navbar = new Navbar('../img/logo.png')
   let projects = new DropDown('Projects')
   projects.links.push(new NavLink(
-    './welcome',
+    '../welcome',
     'A page containing useful links for getting up to speed after getting a new computer.',
     'Welcome'
   ))
   projects.links.push(new NavLink(
-    './clock',
+    '../clock',
     'A simple alarm clock',
     'Clock'
   ))
-  let about = new NavLink('./about', '', 'About')
-  let contact = new NavLink('./contact', '', 'Contact')
+  let about = new NavLink('../about', '', 'About')
+  let contact = new NavLink('../contact', '', 'Contact')
   navbar.elements.push(projects)
   navbar.elements.push(about)
   navbar.elements.push(contact)
