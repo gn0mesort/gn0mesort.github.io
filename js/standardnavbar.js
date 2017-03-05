3ea419adc5bdd457b5cbf4d20d1316f9bbb0628c
gn0mesort <gnomesort@megate.ch>
2017-03-05 14:55:58 -0800

    Signed-off-by: gn0mesort <gnomesort@megate.ch>

--- js/standardnavbar.js
+++ js/standardnavbar.js
@@ -1,26 +1,23 @@
-$.getScript('../js/navbar.js', function () {
-  let navbar = new Navbar('../img/logo.png')
-  let projects = new DropDown('Projects')
-  projects.links.push(new NavLink(
-    '../welcome',
-    'A page containing useful links for getting up to speed after getting a new computer.',
-    'Welcome'
-  ))
-  projects.links.push(new NavLink(
-    '../clock',
-    'A simple alarm clock',
-    'Clock'
-  ))
-  let about = new NavLink('../about', '', 'About')
-  let contact = new NavLink('../contact', '', 'Contact')
-  navbar.elements.push(projects)
-  navbar.elements.push(about)
-  navbar.elements.push(contact)
+let navbar = new Navbar('../img/logo.png')
+let projects = new DropDown('Projects')
+projects.links.push(new NavLink(
+  '../welcome',
+  'A page containing useful links for getting up to speed after getting a new computer.',
+  'Welcome'
+))
+projects.links.push(new NavLink(
+  '../clock',
+  'A simple alarm clock',
+  'Clock'
+))
+let about = new NavLink('../about', '', 'About')
+let contact = new NavLink('../contact', '', 'Contact')
+navbar.elements.push(projects)
+navbar.elements.push(about)
+navbar.elements.push(contact)
 
-
-  navbar.appendNavbar($('#navbar'))
-  navbar.handleNavbar($('#navbar'))
-  $('#navbar ul').attr('class', 'glow')
-  $('#navbar button').attr('class', 'dropdownbtn monospace green glow')
-  $('#navbar div[class="dropdowncont"]').attr('class', 'dropdowncont transbox')
-})
+navbar.appendNavbar($('#navbar'))
+navbar.handleNavbar($('#navbar'))
+$('#navbar ul').attr('class', 'glow')
+$('#navbar button').attr('class', 'dropdownbtn monospace green glow')
+$('#navbar div[class="dropdowncont"]').attr('class', 'dropdowncont transbox')
