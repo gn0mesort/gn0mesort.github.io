a53ae266b0948e5003998fa35500785a6e23a69d
gn0mesort <gnomesort@megate.ch>
2017-03-05 17:13:32 -0800

    Signed-off-by: gn0mesort <gnomesort@megate.ch>

--- js/standardnavbar.js
+++ js/standardnavbar.js
@@ -20,9 +20,9 @@ navbar.appendNavbar($('#navbar'))
 navbar.handleNavbar($('#navbar'))
 $('#navbar ul').attr('class', 'glow')
 $('#navbar button').attr('class', 'dropdownbtn monospace green glow')
-$('#navbar div[class="dropdowncont"]').attr('class', 'dropdowncont transbox')
+// $('#navbar div[class="dropdowncont"]').attr('class', 'dropdowncont transbox')
 $('#navbar nav[class="dropdown"]').on('mouseenter.navbar', function () {
-  $('#navbar #navigation').css('height', '15vw')
+  $('#navbar #navigation').css('height', '5vw')
   console.log('focus')
 })
 $('#navbar nav[class="dropdown"]').on('mouseleave.navbar', function () {
