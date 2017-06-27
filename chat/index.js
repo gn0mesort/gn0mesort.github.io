facdd4322438a38d497d8a52641bda7922fad69f
gn0mesort <gnomesort@megate.ch>
2017-06-26 23:14:11 -0700

    Add chat

---
+++ chat/index.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="../css/default.css" type="text/css" />
+    <link rel="stylesheet" href="../css/megatech.css" type="text/css" />
+    <link rel="icon" href="../img/favicon.png" />
+    <title>メガテクボディ合同会社</title>
+  </head>
+  <body style="background-image:url('../img/background.jpg')">
+    <div class="navbar"></div>
+    <div class="space"></div>
+    <div class="container green">
+        <div class="column column-one">
+            <div class="content-box">
+                <script id="cid0020000159534966884" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"megatechbody","arch":"js","styles":{"a":"00FF00","b":100,"c":"000000","d":"000000","k":"00FF00","l":"00FF00","m":"00FF00","p":"10","q":"00FF00","r":100,"usricon":0.94,"cnrs":"0.58"}}</script>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="../js/index.js"></script>
+    <script>
+      var m = new Megatech()
+      $('div[class="navbar"]').load('../html/navbar.html', function() {
+        var navbar = $('div[class="navbar"]')[0]
+        m.handleNavBar(navbar)
+      })
+    </script>
+  </body>
+</html>
