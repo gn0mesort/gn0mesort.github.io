c29503d3d6fd5330c49fd8c438416e79f62ad5cd
Alexander Rothman <gnomesort@megate.ch>
2021-05-11 15:48:17 -0700

    Improve build script.

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 temp=$(mktemp -d -p /tmp/)
+dest=$(pwd)
 hugo -d $temp
 git checkout master
 GLOBIGNORE=.git
 rm -rfv *
 unset GLOBIGNORE
-cp -r $temp .
+cd $temp
+for item in *; do
+  cp -rv $item "$dest/$item"
+done
 rm -rf $temp
