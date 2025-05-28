fd635613bb6cf521212e5bff69b8cca3583c37ba
Alexander Rothman <gnomesort@megate.ch>
2025-05-27 21:34:31 -0700

    Fixup Exercise Files

--- static/python/ex2.py
+++ static/python/ex2.py
@@ -1,57 +1,56 @@
-"""
-ageguesser.py
-
-The goal of this program is to guess the age of the user.
-
-Your program should have the following features:
-
-1. It should calculate a guess and ask the user if it's correct.
-2. If the guess isn't correct, the program should ask the user if their age is greater than or less than the guess.
-3. After an incorrect guess, it should calculate a new guess and try again.
-4. The program should track how many guesses it makes and report how it did once it guesses correctly
-5. If the program can't figure out the user's age, it should print a special message.
-6. Once you have a working program, think about how many guesses you should really need. Can you make your program a better age guesser?
-
-Example Input and Output:
-
-Is your age 58? (Enter Y for yes and N for no): N
-Is your age greater than 58? (Enter Y for yes and N for no): N
-Is your age 29? (Enter Y for yes and N for no): N
-Is your age greater than 29? (Enter Y for yes and N for no): N
-Is your age 15? (Enter Y for yes and N for no): Y
-Your age was 15! I needed 3 guesses to figure it out!
-
-Is your age 58? (Enter Y/N) N
-Is your age greater than 58? Enter (Y/N) Y
-Is your age 86? (Enter Y/N) N
-Is your age greater than 86? Enter (Y/N) Y
-Is your age 100? (Enter Y/N) N
-Is your age greater than 100? Enter (Y/N) N
-Is your age 93? (Enter Y/N) Y
-Your age was 93! I needed 4 guesses to figure it out!
-
-Is your age 58? (Enter Y/N) n
-Is your age greater than 58? Enter (Y/N) n
-Is your age 29? (Enter Y/N) n
-Is your age greater than 29? Enter (Y/N) n
-Is your age 15? (Enter Y/N) n
-Is your age greater than 15? Enter (Y/N) n
-Is your age 8? (Enter Y/N) n
-Is your age greater than 8? Enter (Y/N) n
-Is your age 4? (Enter Y/N) n
-Is your age greater than 4? Enter (Y/N) n
-Is your age 2? (Enter Y/N) n
-Is your age greater than 2? Enter (Y/N) n
-Is your age 1? (Enter Y/N) n
-Is your age greater than 1? Enter (Y/N) n
-I couldn't guess your age, are you cheating!?
-
-Hints:
-
-- Use the example output as a guide. Is there a pattern to how the program is guessing? How does it choose its guesses?
-- Recall that strings in Python have their own built in functionality called methods. You can use string methods to check if the input starts with "Y" or "N"
-- Think about how old the oldest person is and how old the youngest person is. Is anyone older than 120? Is anyone younger than 1?
-
-"""
+##
+# ageguesser.py
+#
+# The goal of this program is to guess the age of the user.
+#
+# Your program should have the following features:
+#
+# 1. It should calculate a guess and ask the user if it's correct.
+# 2. If the guess isn't correct, the program should ask the user if their age is greater than or less than the guess.
+# 3. After an incorrect guess, it should calculate a new guess and try again.
+# 4. The program should track how many guesses it makes and report how it did once it guesses correctly
+# 5. If the program can't figure out the user's age, it should print a special message.
+# 6. Once you have a working program, think about how many guesses you should really need. Can you make your program a better age guesser?
+#
+# Example Input and Output:
+#
+# Is your age 58? (Enter Y for yes and N for no): N
+# Is your age greater than 58? (Enter Y for yes and N for no): N
+# Is your age 29? (Enter Y for yes and N for no): N
+# Is your age greater than 29? (Enter Y for yes and N for no): N
+# Is your age 15? (Enter Y for yes and N for no): Y
+# Your age was 15! I needed 3 guesses to figure it out!
+#
+# Is your age 58? (Enter Y/N) N
+# Is your age greater than 58? Enter (Y/N) Y
+# Is your age 86? (Enter Y/N) N
+# Is your age greater than 86? Enter (Y/N) Y
+# Is your age 100? (Enter Y/N) N
+# Is your age greater than 100? Enter (Y/N) N
+# Is your age 93? (Enter Y/N) Y
+# Your age was 93! I needed 4 guesses to figure it out!
+#
+# Is your age 58? (Enter Y/N) n
+# Is your age greater than 58? Enter (Y/N) n
+# Is your age 29? (Enter Y/N) n
+# Is your age greater than 29? Enter (Y/N) n
+# Is your age 15? (Enter Y/N) n
+# Is your age greater than 15? Enter (Y/N) n
+# Is your age 8? (Enter Y/N) n
+# Is your age greater than 8? Enter (Y/N) n
+# Is your age 4? (Enter Y/N) n
+# Is your age greater than 4? Enter (Y/N) n
+# Is your age 2? (Enter Y/N) n
+# Is your age greater than 2? Enter (Y/N) n
+# Is your age 1? (Enter Y/N) n
+# Is your age greater than 1? Enter (Y/N) n
+# I couldn't guess your age, are you cheating!?
+#
+# Hints:
+#
+# - Use the example output as a guide. Is there a pattern to how the program is guessing? How does it choose its guesses?
+# - Recall that strings in Python have their own built in functionality called methods. You can use string methods to check if the input starts with "Y" or "N"
+# - Think about how old the oldest person is and how old the youngest person is. Is anyone older than 120? Is anyone younger than 1?
+#
 
 # Your code goes below here!
--- static/python/ex3.py
+++ static/python/ex3.py
@@ -1,92 +1,92 @@
-"""
-box.py
+##
+# box.py
+#
+# The goal of this program is to draw boxes using the provided utilities.
+#
+# Your program should have the following features:
+#
+# 1. It should ask the user how wide their terminal is in characters (80 is a safe size)
+# 2. It should ask the user how tall their terminal is in characters (24 is a safe size)
+# 3. It should create an Image object like: Image(width, height)
+# 4. It should provide a complete box() function that draws a box into an Image
+# 5. It should draw at least two boxes into the terminal
+# 6. Can you draw other shapes? Could you use the structure of box() to create a triangle() function or a circle function()? Can you write text into your boxes?
+#
+# Example input and output:
+#
+# How wide is your display? 80
+# How tall is your display? 24
+# *---------*
+# |         |
+# | *--*    |
+# | |  |    |
+# | |  |    |
+# | *--*    |
+# |         |
+# |         |
+# |         |
+# |        *----------*
+# *--------|          |
+#          |          |
+#          |          |
+#          |          |
+#          |          |
+#          |          |
+#          |          |
+#          |          |
+#          |          |
+#          |          |
+#          *----------*
+#
+#
+# How wide is your display? 80
+# How tall is your display? 24
+# *---------*
+# |         |
+# |         |
+# |         |
+# |         |
+# |    *    |
+# |         |
+# |         |
+# |         |
+# |         |
+# *---------*
+#
+#
+# How wide is your display? 80
+# How tall is your display? 24
+# *------------------------------------------------------------------------------*
+# |                                                                              |
+# |                                                                              |
+# |                                                                              |
+# |                                                                              |
+# |                                                                              |
+# |                                 *---------*                                  |
+# |                                 |         |                                  |
+# |                                 |         |                                  |
+# |                                 |         |                                  |
+# |                                 |         |                                  |
+# |                                 |         |                                  |
+# |                                 |         |                                  |
+# |                                 |         |                                  |
+# |                                 |         |                                  |
+# |                                 |         |                                  |
+# |                                 *---------*                                  |
+# |                                                                              |
+# |                                                                              |
+# |                                                                              |
+# |                                                                              |
+# |                                                                              |
+# |                                                                              |
+# *------------------------------------------------------------------------------*
+#
+# Hints:
+#
+# - Think of your Image like a piece of graph paper with the origin in the upper left corner and the y-axis getting more positive (rather than more negative) as you go down the page
+# - Look at the structure of Image.display() can you use for-loops to draw boxes in the same way?
+#
 
-The goal of this program is to draw boxes using the provided utilities.
-
-Your program should have the following features:
-
-1. It should ask the user how wide their terminal is in characters (80 is a safe size)
-2. It should ask the user how tall their terminal is in characters (24 is a safe size)
-3. It should create an Image object like: Image(width, height)
-4. It should provide a complete box() function that draws a box into an Image
-5. It should draw at least two boxes into the terminal
-6. Can you draw other shapes? Could you use the structure of box() to create a triangle() function or a circle function()? Can you write text into your boxes?
-
-Example input and output:
-
-How wide is your display? 80
-How tall is your display? 24
-*---------*                                                                     
-|         |                                                                     
-| *--*    |                                                                     
-| |  |    |                                                                     
-| |  |    |                                                                     
-| *--*    |                                                                     
-|         |                                                                     
-|         |                                                                     
-|         |                                                                     
-|        *----------*                                                           
-*--------|          |                                                           
-         |          |                                                           
-         |          |                                                           
-         |          |                                                           
-         |          |                                                           
-         |          |                                                           
-         |          |                                                           
-         |          |                                                           
-         |          |                                                           
-         |          |                                                           
-         *----------* 
-
-
-How wide is your display? 80
-How tall is your display? 24
-*---------*                                                                     
-|         |                                                                     
-|         |                                                                     
-|         |                                                                     
-|         |                                                                     
-|    *    |                                                                     
-|         |                                                                     
-|         |                                                                     
-|         |                                                                     
-|         |                                                                     
-*---------*       
-
-
-How wide is your display? 80
-How tall is your display? 24
-*------------------------------------------------------------------------------*
-|                                                                              |
-|                                                                              |
-|                                                                              |
-|                                                                              |
-|                                                                              |
-|                                 *---------*                                  |
-|                                 |         |                                  |
-|                                 |         |                                  |
-|                                 |         |                                  |
-|                                 |         |                                  |
-|                                 |         |                                  |
-|                                 |         |                                  |
-|                                 |         |                                  |
-|                                 |         |                                  |
-|                                 |         |                                  |
-|                                 *---------*                                  |
-|                                                                              |
-|                                                                              |
-|                                                                              |
-|                                                                              |
-|                                                                              |
-|                                                                              |
-*------------------------------------------------------------------------------*
-
-Hints:
-
-- Think of your Image like a piece of graph paper with the origin in the upper left corner and the y-axis getting more positive (rather than more negative) as you go down the page
-- Look at the structure of Image.display() can you use for-loops to draw boxes in the same way?
-
-"""
 
 
 ##################################
