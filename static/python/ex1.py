e96641fba87c958ad6a2b6c71811b355480433ae
Alexander Rothman <gnomesort@megate.ch>
2025-05-27 21:19:12 -0700

    Addd Exercises for Tomorrow's Demo

---
+++ static/python/ex1.py
@@ -0,0 +1,60 @@
+"""
+cowsay.py
+
+The goal of this program is to ask the user for a message, and then display that in a speech bubble attached to the cow.
+
+Your program should have the following features:
+
+1. It should ask the user to type in at least one message.
+2. It should display the user's message in a speech bubble.
+3. After displaying the user's message it should display the cow graphic.
+4. Once you have this working customize it! Can you add your own graphic? Can you ask more than one question? Can you change the speech bubble into a thought bubble?
+
+Example Input and Output:
+
+
+What should I say? Hello!
+  ------  
+*        *
+| Hello! |
+*        *
+  ------ 
+
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+
+What should I say? Python is powerful... and fast;plays well with others;runs everywhere;is friendly & easy to learn;is Open.
+  -----------------------------------------------------------------------------------------------------------  
+*                                                                                                             *
+| Python is powerful... and fast;plays well with others;runs everywhere;is friendly & easy to learn;is Open.  |
+*                                                                                                             *
+  ----------------------------------------------------------------------------------------------------------- 
+
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+Hints:
+
+- Start by writing print(cow) and then put the rest of your program above that.
+- Remember that you can add two pieces of text together with + and repeat a piece of text with *
+
+"""
+
+# This string that begins and ends with """ is a special string in Python. These triple quoted strings can take up multiple lines and are stored exactly as they appear in the program.
+# So, this cow will look exactly right if we just print(cow)
+cow = """
+    \\   ^__^
+     \\  (oo)\\_______
+        (__)\\       )\\/\\
+            ||----w |
+            ||     ||
+"""
+
+# Your code goes below here!
